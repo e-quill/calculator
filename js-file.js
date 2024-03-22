4d296d616f8a47a3640c1a882fdddc87d923ebda
e-quill <e-quill@users.noreply.github.com>
2024-03-22 15:43:11 +0530

    Add arithmetic functions

---
+++ js-file.js
@@ -0,0 +1,23 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function operate(firstNumber,secondNumber,operator){
+ return add(firstNumber,secondNumber)
+}
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
